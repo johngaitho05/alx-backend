3af3eecbcb9236d429b96ed28be2ca41d7b5fe6c
John Gaitho <johngaitho05@gmail.com>
2024-04-01 15:51:37 +0300

    csv pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,12 +4,14 @@
 import csv
 from typing import List, Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns the indices of the given page"""
     x, y = 0, page_size
     for i in range(1, page):
         x, y = x + page_size, y + page_size
     return x, y
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -32,7 +34,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Returns truncated csv data based on pagination info"""
-        assert (type(page) is int and  type(page_size) is int
+        assert (type(page) is int and type(page_size) is int
                 and page > 0 and page_size > 0)
         x, y = index_range(page, page_size)
         return self.dataset()[x:y]
