b4483d872a06723374d0162bb9bf6842ce39ec65
John Gaitho <johngaitho05@gmail.com>
2024-04-09 22:28:10 +0300

    set up flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello_world():
     """ Prints a Message when / is called """
     return render_template('0-index.html')
 
