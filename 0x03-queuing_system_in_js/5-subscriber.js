0ba6ce804710eaff19519901c99ba945424de67e
John Gaitho <johngaitho05@gmail.com>
2024-05-08 13:49:16 +0300

    sending and receiving messages on channels

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+const redis = require('redis');
+
+const subscriber = redis.createClient();
+
+subscriber.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+subscriber.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+subscriber.on('message', (channel, message) => {
+  console.log(`Received message from channel ${channel}: ${message}`);
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe('holberton school channel');
+    subscriber.quit();
+  }
+});
+
+subscriber.subscribe('holberton school channel');
