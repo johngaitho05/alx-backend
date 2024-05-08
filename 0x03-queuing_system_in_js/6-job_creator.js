693ddc4687f2063d624138e161c88c5fd04b85bb
John Gaitho <johngaitho05@gmail.com>
2024-05-08 14:05:25 +0300

    create and process jobs using kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,27 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'Your notification code is XYZ123'
+};
+
+const job = queue.create('push_notification_code', jobData);
+
+job.on('enqueue', () => {
+  console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
+
+job.save((err) => {
+  if (err) {
+    console.error(`Error creating job: ${err}`);
+  }
+});
